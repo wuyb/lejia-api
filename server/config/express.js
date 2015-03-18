d05b84f8eaab47ef095742f423532855a3cc6b65
Yanbo Wu <yanbo.wu@gmail.com>
2015-03-18 19:11:41 +0800

    add Authorization to allowed header

--- server/config/express.js
+++ server/config/express.js
@@ -32,7 +32,7 @@ module.exports = function(app) {
   app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Authorization');
     next();
   });
 
